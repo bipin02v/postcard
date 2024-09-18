68beb3a1fe2a1c72d4c9c43eaea86813fc3066c6
Bipin Patel <bipin.02v@gmail.com>
2024-09-18 12:57:03 +0530

    images add

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
-import img1 from "./assets/img1.jpg";
-import img2 from "./assets/img2.jpg";
-import img3 from "./assets/img3.jpg";
+import img1 from "./assets/img1.webp";
+import img2 from "./assets/img2.webp";
+import img3 from "./assets/img3.webp";
 import MainCard from "./components/MainCard";
 import Card from "./components/Card";
 import CardImage from "./components/CardImage";
